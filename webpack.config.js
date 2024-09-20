6cc628f77d570f1b9e84c1d71be7fe73ae3e53c2
Rohan1243 <125541724+Rohan1243@users.noreply.github.com>
2024-09-20 19:00:01 +0530

    updated dependencies in package json

--- webpack.config.js
+++ webpack.config.js
@@ -19,17 +19,16 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [
-              ['@babel/env', // This compiles es6, 7, 8, 9, 10 down to es5 - Supports lates javascript standard given in ecma script.
+              ['@babel/env', 
                 {
-                  useBuiltIns: 'usage', // alternative mode: "entry"
-                  corejs: 3, // default would be 2
+                  useBuiltIns: 'usage',
+                  corejs: 3,
                   targets: '> 0.25%, not dead',
-                // set your own target environment here (see Browserslist)
                 },
               ],
             ],
             plugins: [
-              '@babel/plugin-proposal-class-properties', // This is for using features like class properties for the ability to directly declare properties inside the class and not the constructor. It is used by react.
+              '@babel/plugin-proposal-class-properties',
             ],
           },
         },
@@ -37,16 +36,36 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          'style-loader', // Adds the style to the DOM
-          'css-loader', // Reads the CSS
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require('autoprefixer')(),
+                ],
+              },
+            },
+          },
         ],
-      },
+      }
+      
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(), // Cleans the dist folder before the build
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: 'index.html',
-    }), // Generates HTML file along with the other files
+    }),
   ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 8080, 
+    hot: true,  
+    open: true, 
+  },
 };
